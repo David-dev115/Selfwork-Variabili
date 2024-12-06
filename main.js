b7cb9e80afc015a031db24adfd4a16f82b241acb
David Paparo <d.paparo@tiscali.it>
2024-12-06 17:00:15 +0100

    esercizio svolto Selfwork Variabili

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+let num1 = 50;
+
+let num2 = 13;
+
+let messaggio1 = `Il valore della variabile num1 e' `;
+
+let messaggio2 = `Il valore della variabile num2 e' `;
+
+console.log( messaggio1 + num1 );
+
+
+console.log( messaggio2 + num2 );
+
+
+let stringa = `alto`
+
+// sostituzione variabile "stringa"
+stringa = `basso`
+
+let messaggio3 = `Il valore della variabile "stringa" è `;
+
+console.log( messaggio3 + stringa );
+
+const PIGRECO = 3.14159265359;
+
+// se cambio il valore della COSTANTE visualizzo errore sulla Console : Uncaught  TypeError: Assignment to constant variable.
+// PIGRECO = 5;
+
+let messaggio4 = `Il valore della costante "PIGRECO" è `;
+
+console.log( messaggio4 + PIGRECO );
+
+
